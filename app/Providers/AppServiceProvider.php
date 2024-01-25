0957c775eb2bd174fd08737d3e76a9439017cf94
victor braz <victor.90braz@gmail.com>
2024-01-25 14:34:01 +0100

    App\Providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        $this->app->bind(CurrencyInterface::class, AmdorenService::class);
+        //$this->app->bind(CurrencyInterface::class, AmdorenService::class);
         //$this->app->bind(CurrencyInterface::class, FixerService::class);
     }
 
