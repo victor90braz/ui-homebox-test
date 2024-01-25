ede466f2c6b27ffefbed188996e8a7f3fc21f5fc
victor braz <victor.90braz@gmail.com>
2024-01-25 16:37:37 +0100

    VERSION UPDATED 1.1

--- routes/web.php
+++ routes/web.php
@@ -2,22 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
-use Illuminate\Routing\Middleware\SubstituteBindings;
-
-Route::get('/currency/api/convert', [\App\Http\Controllers\CurrencyController::class, 'convert'])
-    ->withoutMiddleware(SubstituteBindings::class);
+Route::get('/currency/api/convert', [\App\Http\Controllers\CurrencyController::class, 'convert']);
