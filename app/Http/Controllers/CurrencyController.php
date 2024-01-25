6d9cf6d8d66c9db75c81b318c35c8a75865534bd
victor braz <victor.90braz@gmail.com>
2024-01-25 19:43:16 +0100

    create a route and methods for AmdorenService and FixerService

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -1,6 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Services\AmdorenService;
+use App\Services\FixerService;
+
 interface CurrencyInterface
 {
     /**
@@ -30,6 +33,54 @@ class CurrencyController extends Controller
         $amount = $validateData['amount'];
 
         $response = $currency->convert($from, $to, $amount);
+
+        return [
+            'converted' => $response['amount'],
+            'currency' => $response['to'],
+        ];
+    }
+
+    /**
+     * @param AmdorenService $amdorenService
+     * @return array
+     */
+    public function convertAmdoren(AmdorenService $amdorenService)
+    {
+        $validateData = request()->validate([
+            'from' => ['required', 'string', 'size:3'],
+            'to' => ['required', 'string', 'size:3'],
+            'amount' => ['required', 'numeric'],
+        ]);
+
+        $from = $validateData['from'];
+        $to = $validateData['to'];
+        $amount = $validateData['amount'];
+
+        $response = $amdorenService->convert($from, $to, $amount);
+
+        return [
+            'converted' => $response['amount'],
+            'currency' => $response['to'],
+        ];
+    }
+
+    /**
+     * @param FixerService $fixerService
+     * @return array
+     */
+    public function convertFixer(FixerService $fixerService)
+    {
+        $validateData = request()->validate([
+            'from' => ['required', 'string', 'size:3'],
+            'to' => ['required', 'string', 'size:3'],
+            'amount' => ['required', 'numeric'],
+        ]);
+
+        $from = $validateData['from'];
+        $to = $validateData['to'];
+        $amount = $validateData['amount'];
+
+        $response = $fixerService->convert($from, $to, $amount);
 
         return [
             'converted' => $response['amount'],
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,5 @@ Route::get('/', function () {
 });
 
 Route::get('/currency/api/convert', [\App\Http\Controllers\CurrencyController::class, 'convert']);
+Route::get('/currency/api/convert-amdoren', [\App\Http\Controllers\CurrencyController::class, 'convertAmdoren']);
+Route::get('/currency/api/convert-fixer', [\App\Http\Controllers\CurrencyController::class, 'convertFixer']);
