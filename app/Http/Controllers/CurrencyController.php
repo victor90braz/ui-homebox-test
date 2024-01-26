fdc5d8b35d144295c622762eb801b5b28c9e0370
victor braz <victor.90braz@gmail.com>
2024-01-26 14:35:25 +0100

    export CurrentInterface

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -2,16 +2,6 @@
 
 namespace App\Http\Controllers;
 
-interface CurrencyInterface
-{
-    /**
-     * @param string $from
-     * @param string $to
-     * @param float $amount
-     * @return array
-     */
-    public function convert(string $from, string $to, float $amount): array;
-}
 class CurrencyController extends Controller
 {
     const QUERY_PARAMETER_AS_DEFAULT = 'GBP';
