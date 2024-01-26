a1d148ba20ce1128c056daabcec79bbdc6d4da23
victor braz <victor.90braz@gmail.com>
2024-01-26 15:58:32 +0100

    CODE REVIEW INTERFACE IMPLEMENTATION

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,4 @@ Route::get('/', function () {
 Route::get('/currency/api/convert-amdoren', [AmdorenController::class, 'convert'])->name('currency.convert.amdoren');
 Route::get('/currency/api/convert-fixer', [FixerController::class, 'convert'])->name('currency.convert.fixer');
 
-
+// http://127.0.0.1:8000/currency/api/convert-amdoren?amount=33&to=EUR
